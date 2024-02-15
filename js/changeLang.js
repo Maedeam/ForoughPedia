a9581f109d8e9bb9b676eb7575653f7cb1e4ab1d
Maede <me.hajjari@gmail.com>
2024-02-15 10:19:11 +0100

    feb14

--- js/changeLang.js
+++ js/changeLang.js
@@ -1,19 +1,22 @@
 function changeLanguage(lang) {
+  var currentScript = document.baseURI;
 
-    var currentScript = document.baseURI;
- 
-    if (currentScript) {
+  if (currentScript) {
+    var currentScriptPath = currentScript;
+    var currentScriptPathSplit = currentScriptPath.split("/");
 
-        var currentScriptPath = currentScript;
-        var currentScriptPathSplit = currentScriptPath.split('/');
+    var currentScriptFolderPath = currentScriptPath.substring(
+      0,
+      currentScriptPath.lastIndexOf("/") + 1
+    );
 
-        var currentScriptFolderPath = currentScriptPath.substring(0, currentScriptPath.lastIndexOf('/') + 1);
-        
-        var languageRegex = /\/(En|Fa|It)\//;
+    var languageRegex = /\/(en|fa|it)\//;
 
-        var newLanguage = lang;
-        var newURL = currentScriptFolderPath.replace(languageRegex, '/' + newLanguage + '/') + currentScriptPathSplit[currentScriptPathSplit.length - 1];
+    var newLanguage = lang;
+    var newURL =
+      currentScriptFolderPath.replace(languageRegex, "/" + newLanguage + "/") +
+      currentScriptPathSplit[currentScriptPathSplit.length - 1];
 
-        window.location.href = newURL;
-    }
+    window.location.href = newURL;
+  }
 }
